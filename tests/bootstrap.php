5f718c62a0a675e19a38676c6d08266e3a57ff9e
Côme Chilliet <come.chilliet@nextcloud.com>
2025-05-26 14:28:15 +0200

    chore(tests): Cleanup bootstrap.php to be forward-compatible
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,20 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+use OCP\App\IAppManager;
+use OCP\Server;
 
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 
-// Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-
-// Fix for "Autoload path not allowed: .../calendar_resource_management/tests/testcase.php"
-\OC_App::loadApp('calendar_resource_management');
-
-OC_Hook::clear();
+Server::get(IAppManager::class)->loadApp('calendar_resource_management');
